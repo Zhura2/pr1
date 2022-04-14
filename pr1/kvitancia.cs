03ead0a73d77649bd4d2c4644879aea2d5ac55ee
zuravlevnikita072@gmail.com <zuravlevnikita072@gmail.com>
2022-04-14 11:18:55 +0300

    Добавил класс

---
+++ pr1/kvitancia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pr1
+{
+    class kvitancia
+    {
+        public int Nomer { get; set; }
+        public string Data { get; set; }
+        public float Summa { get; set; }
+    }   
+}
