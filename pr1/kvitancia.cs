07cb4f52c3c6325d4398f0d024caeaa2e3a0aabc
zuravlevnikita072@gmail.com <zuravlevnikita072@gmail.com>
2022-04-14 12:13:20 +0300

    Добавили конструкторы класса

--- pr1/kvitancia.cs
+++ pr1/kvitancia.cs
@@ -9,6 +9,18 @@ namespace pr1
         public int Nomer { get; set; }
         public string Data { get; set; }
         public float Summa { get; set; }
+        public Kvitancia()
+        {
+            Nomer = 0;
+            Data = "";
+            Summa = 0;
+        }
+    public Kvitancia(int nomer, string data, float summa)
+    {
+        this.Nomer = nomer;
+        this.Data = data;
+        this.Summa = summa;
+    }
 
 }
 }
