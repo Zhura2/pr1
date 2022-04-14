a950a3bb7131964144cba00f87f8c52147376707
zuravlevnikita072@gmail.com <zuravlevnikita072@gmail.com>
2022-04-14 12:16:10 +0300

    Добавили метод  main

--- pr1/Program.cs
+++ pr1/Program.cs
@@ -6,7 +6,32 @@ namespace pr1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Kvitancia a = new Kvitancia();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1 - Добавить квитанцию" + " " +
+                "2 - Вывести квитанцию" + " " + "3 - Выход");
+                int number = Convert.ToInt32(Console.ReadLine());
+                switch (number)
+                {
+                    case 1:
+                        {
+                            a.Input();
+                            break;
+                        }
+                    case 2:
+                        {
+                            a.Print();
+                            break;
+                        }
+                    default:
+                        {
+                            flag = false;
+                            break;
+                        }
+                }
+            }
         }
     }
 }
